d39f0f3fbbd349df19e07d62d3d636a2d413a9e7
angie <angiewheeler99@gmail.com>
2025-11-11 19:34:32 -0700

    committing changes to add to week 01 assignment.

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
-//create routes
+//create routes for endpoints
 router.get('/', usersController.getAll);
 
 router.get('id', usersController.getSingle);
+
+router.post('/', usersController.createUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 //Middleware
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
-//app.get('/', (req, res) => {
-//  res.send('Server is running!');
-//});
+//multiple sites for swagger for testing for headers
+app.use((req, res, next) => {
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
  mongodb.initDb((err) => {
      if (err) {
@@ -18,7 +25,9 @@ app.use('/', require('./routes'));
      else {
          app.listen(port, () => console.log(`Database is listening and node is running on port ${port}`));
      }
-})
+ })
+
+
 
 
 
